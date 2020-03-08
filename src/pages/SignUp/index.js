aa272e80d1bece3cfc05c72f45e3b90430a18cec
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-08 19:21:25 -0300

    Implementacao de signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/prefer-stateless-function */
 import { Form, Input } from '@rocketseat/unform';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
@@ -16,11 +18,17 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form schema={schema}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
