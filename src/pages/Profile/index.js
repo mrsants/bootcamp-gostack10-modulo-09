2c9a717e7fc18b0a6e1a7a6caef967a8a7669c40
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-08 01:09:14 -0300

    Base da home e do perfil

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Profile extends Component {
+  render() {
+    return <div />;
+  }
+}
