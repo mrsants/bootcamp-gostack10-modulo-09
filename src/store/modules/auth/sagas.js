69fc7b85ce956df086205d0017e0c12bd51b2310
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-08 19:24:27 -0300

    Implementacao de signout

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,10 +1,12 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
-import api from '../../../services/api';
-import history from '../../../services/history';
-import { signFailure, signInSuccess } from './actions';
 
-function* signIn({ payload }) {
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
   try {
     const { email, password } = payload;
 
@@ -16,19 +18,57 @@ function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário nao é prestador');
-      toast.error('Usuário nao é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
     toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
